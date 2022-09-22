f11a189e05ab04a9d9b463b3b2d6da252c42176c
ArmandoFigueiredo <armandopfigueiredo@gmail.com>
2022-09-22 19:31:51 -0300

    first commit

--- routes/consumer.routes.js
+++ routes/consumer.routes.js
@@ -1,5 +1,29 @@
 import express from "express";
+import { ConsumerModel } from "../models/consumer.model"
 
 const consumerRouter = express.Router();
 
+consumerRouter.post("/", async (req, res) => {
+    try {
+      const createdConsumer = await ConsumerModel.create(req.body);
+  
+      return res.status(201).json(createdConsumer);
+    } catch (err) {
+      console.log(err);
+      return res.status(501).json(err);
+    }
+  });
+
+  consumerRouter.get("/:id", async (req, res) => {
+    try {      
+      const user = await ConsumerModel.findOne({ _id: req.params.id });
+  
+      return res.status(200).json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+});
+
 export { consumerRouter };
+
